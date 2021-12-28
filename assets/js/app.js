69ca6339370793433f448c0779d6204979e08613
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-12-27 19:36:09 -0500

    second method complete, project complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,26 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    // console.log(question);
+    const btn = question.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click', function() {
+
+        // This makes only one question stay open, closes other open one! 
+        questions.forEach(function(item) {
+            if(item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+
+
+        question.classList.toggle('show-text');
+    })
+});
+
 // traversing the dom
+/*
 const btns = document.querySelectorAll('.question-btn');
 
 btns.forEach(function(btn) {
@@ -9,3 +30,4 @@ btns.forEach(function(btn) {
         question.classList.toggle('show-text');
     });
 });
+*/
